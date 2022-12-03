5120ea367f3a5db5ad39efc557b191f00d70ef4c
abarhub <abarhub@gmail.com>
2022-12-03 18:26:15 +0100

    debut implémentation (ne marche pas a cause du CORS)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {EMPTY, Observable} from "rxjs";
+import {FormControl, FormGroup} from "@angular/forms";
+import {empty} from "rxjs/internal/Observer";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard-ecowatt';
+  identForm = new FormGroup({
+    secretKey: new FormControl('')
+  });
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  load() {
+
+    console.log('identForm',this.identForm.value);
+    const secretKey=this.identForm.value.secretKey;
+    console.log('secretKey',secretKey);
+    if(secretKey) {
+      console.log('appel getOAuth2');
+      this.getOAuth2(secretKey).subscribe(x => {
+        console.log('oauth2',x);
+        const token = x.access_token;
+        if (token) {
+          console.log('appel ecowatt');
+          this.getEcoWatt().subscribe(x=>{
+            console.log('ecowat:',x);
+          });
+        }
+      });
+    }
+  }
+
+  private getOAuth2(secretKey:string):Observable<OAuth2>{
+    if(secretKey) {
+      const urlOAuth2 = 'https://digital.iservices.rte-france.com/token/oauth/';
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic ' + secretKey
+        })
+      };
+      return this.http.post<OAuth2>(urlOAuth2, null, httpOptions);
+    } else {
+      return EMPTY;
+    }
+  }
+
+  private getEcoWatt(){
+    const urlOAuth2='https://digital.iservices.rte-france.com/open_api/ecowatt/v4/sandbox/signals';
+    return this.http.get<OAuth2>(urlOAuth2);
+  }
+}
+
+export class OAuth2{
+  access_token:string='';
+  token_type:string='';
+  expires_in:number=0;
 }
